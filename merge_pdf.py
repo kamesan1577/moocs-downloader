82f0bf6b55fe649997dc3b24edf62dfc39544728
kamesan <47214420+kamesan1577@users.noreply.github.com>
2024-01-22 19:13:09 +0900

    マージされたPDFの順番が自然な順番になるように変更

--- merge_pdf.py
+++ merge_pdf.py
@@ -2,6 +2,7 @@ import pypdf
 import os
 import sys
 import glob
+from natsort import natsorted
 
 if len(sys.argv) < 2:
     print("引数が足りません")
@@ -23,8 +24,10 @@ merger = pypdf.PdfMerger()
 # ファイルかフォルダかを判定
 if os.path.isfile(SRC_DIR):
     file_list = [SRC_DIR]
+    print("hoge")
 else:
     file_list = glob.glob(SRC_DIR + "/*.pdf")
+file_list = natsorted(file_list)
 print(file_list)
 
 for file in file_list:
